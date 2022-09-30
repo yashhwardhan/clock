4384819233d2c723820067c84a71a911872cbf5a
yashhwardhan <113452366+yashhwardhan@users.noreply.github.com>
2022-09-30 19:37:36 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,17 @@
+// let hrEl = document.getElementsByClassName('hour');
+// let minEl = document.getElementsByClassName('minute');
+// let secEl = document.getElementsByClassName('second');
+
+setInterval(() => {
+    let d = new Date();
+   let htime = d.getHours();
+   let mtime = d.getMinutes();
+   let stime = d.getSeconds();
+   let hrotation = 30*htime + mtime/2;
+   let mrotation = 6*mtime;
+   let srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
